33fec13229bb4a6b69b3243de98355e3da4cb02e
shayanCR07 <shayan1985@gmail.com>
2025-07-03 15:04:36 +0530

    valid anagram

---
+++ String/valid_anagram.java
@@ -0,0 +1,32 @@
+public class valid_anagram {
+    public static void main(String[] args) {
+        String s = "car";
+        String t = "rat";
+        System.out.println(isAnagram(s,t));
+    }
+    public static boolean isAnagram (String s, String t)
+    {
+        int m = s.length();
+        int n = t.length();
+        if(m!=n) return false;
+
+        else{
+            int count[] = new int[26];
+             
+            for(int i = 0; i<m ; i++)
+            {
+                count[s.charAt(i)-'a']++;
+            }
+            for(int i = 0; i<n ; i++)
+            {
+                count[t.charAt(i)-'a']--;
+            }
+            for(int i =0 ; i< count.length; i++)
+            {
+                if(count[i] != 0)return false;
+            }
+           
+        }
+        return true;
+    }
+}
